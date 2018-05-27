a18360d4f56f173fed9b3c1b643a7dd644d87e4c
Harry Taylor <35974116+hdudeness@users.noreply.github.com>
2018-05-27 13:33:18 -0400

    ReadFromFile

---
+++ Creamsicle/ReadFromFile.cs
@@ -0,0 +1,18 @@
+﻿namespace Creamsicle
+{
+    class ReadFromFile
+    {
+        private static void MainFile(string[] args)
+        {
+            string line;
+
+            System.IO.StreamReader hwinfoLog = new System.IO.StreamReader(@"C:\Users\Glitc\Desktop\logs.csv");
+            while((line = hwinfoLog.ReadLine()) != null)
+            {
+                System.Console.WriteLine(line);
+            }
+            hwinfoLog.Close();
+            System.Console.ReadLine();
+        }
+    }
+}
